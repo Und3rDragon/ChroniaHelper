96ed89c4878a54529522c581c0467851a1f8b285
UnderDragon <1306094605@qq.com>
2025-08-22 00:54:56 +0800

    Wrap-up

--- Utils/Shortcuts.cs
+++ Utils/Shortcuts.cs
@@ -2,14 +2,16 @@
 global using MaP = ChroniaHelper.Cores.MapProcessor;
 global using Md = ChroniaHelper.ChroniaHelperModule;
 global using Sav = ChroniaHelper.Modules.ChroniaHelperSaveData;
+global using Ses = ChroniaHelper.Modules.ChroniaHelperSession;
+global using Sh = ChroniaHelper.Utils.Shortcuts;
+using System.Runtime.InteropServices.Marshalling;
 using ChroniaHelper.Utils.ChroniaSystem;
 
-namespace ChroniaHelper.Shortcuts;
+namespace ChroniaHelper.Utils;
 
 public static class Shortcuts
 {
-    public static Dictionary<string, ChroniaFlag> ChroniaFlags(this object obj)
-    {
-        return Md.SaveData.ChroniaFlags;
-    }
+    public static Dictionary<string, ChroniaFlag> CFlags => Md.SaveData.ChroniaFlags;
+    public static Dictionary<string, ChroniaSlider> CSliders => Md.SaveData.ChroniaSliders;
+    public static Dictionary<string, ChroniaCounter> CCounters => Md.SaveData.ChroniaCounters;
 }
