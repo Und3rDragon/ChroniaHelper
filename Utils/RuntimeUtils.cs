abffef7f7bf3f485dd704bcbd3343da7dc721ec2
UnderDragon <1306094605@qq.com>
2025-08-23 02:28:45 +0800

    Input Check from Trigger Trigger by vitellary

---
+++ Utils/RuntimeUtils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChroniaHelper.Utils;
+
+public enum InputTypes
+{
+    Left,
+    Right,
+    Up,
+    Down,
+    Jump,
+    Dash,
+    Grab,
+    Interact,
+    CrouchDash,
+    Any,
+}
+
+public static class RuntimeUtils
+{
+    // Code from vitellery's Trigger Trigger
+    public static bool CheckInput(InputTypes inputType, bool held)
+    {
+        switch (inputType)
+        {
+            case InputTypes.Grab:
+                return (held ? Input.Grab.Check : Input.Grab.Pressed);
+            case InputTypes.Jump:
+                return (held ? Input.Jump.Check : Input.Jump.Pressed);
+            case InputTypes.Dash:
+                return (held ? Input.Dash.Check : Input.Dash.Pressed);
+            case InputTypes.Interact:
+                return (held ? Input.Talk.Check : Input.Talk.Pressed);
+            case InputTypes.CrouchDash:
+                return (held ? Input.CrouchDash.Check : Input.CrouchDash.Pressed);
+            default:
+                Vector2 aim = Input.Aim.Value.FourWayNormal();
+                if (held || (aim != Input.Aim.PreviousValue.FourWayNormal()))
+                {
+                    if (inputType == InputTypes.Left && aim.X < 0f)
+                        return true;
+                    else if (inputType == InputTypes.Right && aim.X > 0f)
+                        return true;
+                    else if (inputType == InputTypes.Down && aim.Y > 0f)
+                        return true;
+                    else if (inputType == InputTypes.Up && aim.Y < 0f)
+                        return true;
+                }
+                break;
+        }
+        return false;
+    }
+}
