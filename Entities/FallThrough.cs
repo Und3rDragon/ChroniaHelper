7ce5d5e17c9453cf04e5da9e63be03541e6f673d
UnderDragon <1306094605@qq.com>
2025-09-06 23:46:29 +0800

    Fall Through from VivHelper

---
+++ Entities/FallThrough.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Celeste;
+using Celeste.Mod.Entities;
+using Monocle;
+using Celeste.Mod;
+using System.Collections;
+using Microsoft.Xna.Framework;
+using ChroniaHelper.Utils;
+
+namespace ChroniaHelper.Entities;
+
+[CustomEntity("ChroniaHelper/FallThrough")]
+public class FallThrough : JumpthruPlatform
+{
+    private float playerDuckTimer = 0;
+    private float FallTime = 0.35f;
+    private bool collidableOverride;
+
+    public FallThrough(EntityData data, Vector2 offset) : base(data, offset)
+    {
+        FallTime = data.Float("FallTime", 0.35f);
+        collidableOverride = data.Bool("collidableOverride", false);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (PlayerUtils.TryGetPlayer(out Player player) && player.Ducking && HasPlayerRider())
+        {
+            playerDuckTimer += Engine.DeltaTime;
+            if (playerDuckTimer >= FallTime)
+            {
+                player.Add(new Coroutine(FallThru(player), true));
+            }
+        }
+        else
+        {
+            playerDuckTimer = 0f;
+        }
+    }
+
+    public IEnumerator FallThru(Player player)
+    {
+        if (player != null && !player.Dead)
+        {
+            GoThrough(player, true);
+        }
+        player.Speed = new Vector2(0f, 60f);
+        yield return 0.25f;
+        while (!PlayerUtils.TryGetAlivePlayer(out player)) //Fixes up some janky stuff
+        {
+            yield return null;
+        }
+        GoThrough(player, false);
+    }
+
+    public void GoThrough(Player p, bool on)
+    {
+        if (collidableOverride)
+        {
+            Collidable = !on;
+        }
+        else
+        {
+            p.IgnoreJumpThrus = on;
+        }
+    }
+}
--- Utils/PlayerUtils.cs
+++ Utils/PlayerUtils.cs
@@ -8,4 +8,20 @@ public static class PlayerUtils
         return (player != null) && (player.Holding != null) && (player.Holding.IsHeld);
     }
 
+    public static Player GetPlayer()
+    {
+        return Engine.Scene?.Tracker?.GetEntity<Player>();
+    }
+    //Thanks to coloursofnoise for this code.
+    public static bool TryGetPlayer(out Player player)
+    {
+        player = Engine.Scene?.Tracker?.GetEntity<Player>();
+        return player != null;
+    }
+    public static bool TryGetAlivePlayer(out Player player)
+    {
+        player = Engine.Scene?.Tracker?.GetEntity<Player>();
+        return !(player?.Dead ?? true);
+    }
+
 }
