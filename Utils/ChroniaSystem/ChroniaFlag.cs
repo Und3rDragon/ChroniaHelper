a40c410c55013ba0ef49ca12773312f9f20f5428
UnderDragon <1306094605@qq.com>
2025-10-02 02:43:08 +0800

    Initial Chronia Flag

--- Utils/ChroniaSystem/ChroniaFlag.cs
+++ Utils/ChroniaSystem/ChroniaFlag.cs
@@ -38,6 +38,7 @@ public class ChroniaFlag
         On.Celeste.Level.Update += OnLevelUpdate;
         On.Monocle.Scene.Update += GlobalUpdate;
         On.Celeste.Level.TransitionRoutine += OnLevelTransition;
+        On.Celeste.Level.Begin += OnLevelBegin;
     }
 
     public static void Unload()
@@ -47,6 +48,24 @@ public class ChroniaFlag
         On.Celeste.Level.Update -= OnLevelUpdate;
         On.Monocle.Scene.Update -= GlobalUpdate;
         On.Celeste.Level.TransitionRoutine -= OnLevelTransition;
+        On.Celeste.Level.Begin -= OnLevelBegin;
+    }
+    
+    public static void OnLevelBegin(On.Celeste.Level.orig_Begin orig, Level self)
+    {
+        orig(self); // Only once when getting in
+
+        MaP.level = self;
+        
+        foreach(var f in Md.SaveData.ChroniaFlags)
+        {
+            // initialize normal flags
+            if (!f.Value.IsNormalFlag && !f.Value.IsCustomFlag)
+            {
+                f.Value.Active = false;
+                f.Key.SetFlag(false);
+            }
+        }
     }
 
     public static IEnumerator OnLevelTransition(On.Celeste.Level.orig_TransitionRoutine orig, Level self, LevelData levelData, Vector2 dir)
@@ -97,27 +116,6 @@ public class ChroniaFlag
     public static void OnLevelUpdate(On.Celeste.Level.orig_Update orig, Level self)
     {
         orig(self);
-        
-        //foreach (var item in Md.SaveData.ChroniaFlags)
-        //{
-        //    // Non-global stuffs only
-        //    if (item.Value.Global) { continue; }
-            
-        //    if (item.Value.Timed > 0f)
-        //    {
-        //        item.Value.Timed = Calc.Approach(item.Value.Timed, 0f, Engine.DeltaTime);
-        //    }
-        //    else if (item.Value.Timed == 0f)
-        //    {
-        //        item.Key.SetFlag(item.Value.ResetTo);
-        //        Md.SaveData.ChroniaFlags.SafeRemove(item.Key);
-        //    }
-
-        //    if (item.Value.Force)
-        //    {
-        //        item.Key.SetFlag(item.Value.Active);
-        //    }
-        //}
     }
 
     public static void GlobalUpdate(On.Monocle.Scene.orig_Update orig, Scene self)
@@ -163,30 +161,6 @@ public class ChroniaFlag
         {
             Md.SaveData.ChroniaFlags.SafeRemove(item);
         }
-
-        //foreach (var item in Md.SaveData.ChroniaFlags)
-        //{
-        //    // Security Check
-        //    item.Value.ChroniaFlagDataCheck();
-
-        //    // Global stuffs only
-        //    if (!item.Value.Global) { continue; }
-
-        //    if (item.Value.Timed > 0f)
-        //    {
-        //        item.Value.Timed = Calc.Approach(item.Value.Timed, 0f, Engine.DeltaTime);
-        //    }
-        //    else if (item.Value.Timed == 0f)
-        //    {
-        //        item.Key.SetFlag(item.Value.ResetTo);
-        //        Md.SaveData.ChroniaFlags.SafeRemove(item.Key);
-        //    }
-
-        //    if (item.Value.Force)
-        //    {
-        //        item.Key.SetFlag(item.Value.Active);
-        //    }
-        //}
     }
     
     public bool Active { get; set; } = false;
@@ -219,6 +193,28 @@ public class ChroniaFlag
         {
             PresetTags.SafeRemove(Labels.Serial);
         }
+    }
+    
+    public void ClearTags()
+    {
+        Tags = new();
+    }
+    
+    public void ClearCustomData()
+    {
+        CustomData = new();
+    }
+    
+    public void ClearPresetTags()
+    {
+        PresetTags = new();
+    }
+    
+    public void ClearAll()
+    {
+        ClearTags();
+        ClearCustomData();
+        ClearPresetTags();
     }
 }
 
