bf314efa7f20b0036fd568bc82af34793bdce5f5
UnderDragon <1306094605@qq.com>
2025-10-03 00:08:54 +0800

    Clear Pack Flag Trigger

---
+++ Triggers/ClearPackedFlagsTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Celeste.Mod.Entities;
+using ChroniaHelper.Cores;
+
+namespace ChroniaHelper.Triggers;
+
+[Tracked(true)]
+[CustomEntity("ChroniaHelper/ClearPackedFlagsTrigger")]
+public class ClearPackedFlagsTrigger : BaseTrigger
+{
+    public ClearPackedFlagsTrigger(EntityData e, Vector2 p) : base(e, p)
+    {
+        labels = e.Attr("labels").Split(',', StringSplitOptions.TrimEntries);
+        mode = e.Int("mode", 0);
+    }
+    private string[] labels;
+    private int mode;
+    private enum Operation
+    {
+        Remove, RemoveTag
+    }
+
+    protected override void OnEnterExecute(Player player)
+    {
+        HashSet<string> toRemove = new();
+        foreach(var item in Md.SaveData.ChroniaFlags)
+        {
+            if (item.Value.PresetTags.Contains(Utils.ChroniaSystem.Labels.Packed))
+            {
+                if (!labels.Contains(item.Value.CustomData["packed_label"])) { continue; }
+                
+                
+            }
+        }
+    }
+}
