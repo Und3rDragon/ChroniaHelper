bd117a39c77733397953619f99ee3ca0e2d48e6e
UnderDragon <1306094605@qq.com>
2025-11-03 03:13:26 +0800

    Message Displayer Trigger -> Zone

---
+++ Entities/MessageDisplayZone.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Celeste.Mod.Entities;
+using ChroniaHelper.Cores;
+using ChroniaHelper.Utils;
+using ChroniaHelper.Utils.ChroniaSystem;
+using IL.MonoMod;
+using Microsoft.Build.Framework;
+
+namespace ChroniaHelper.Entities;
+
+[Tracked(true)]
+[CustomEntity("ChroniaHelper/MessageDisplayZone")]
+public class MessageDisplayZone : BaseEntity
+{
+    public MessageDisplayZone(EntityData d, Vc2 o) : base(d, o)
+    {
+        SerialImage template = new SerialImage(GFX.Game.GetAtlasSubtextures("ChroniaHelper/DisplayFonts/font"));
+
+        template.renderMode = d.Int("renderMode", 0);
+        template.origin = new Vc2(d.Float("lineOriginX", 0.5f), d.Float("lineOriginY", 0.5f));
+        template.segmentOrigin = Vc2.Zero;
+        template.distance = d.Float("letterDistance", 1f);
+        template.color = d.GetChroniaColor("fontColor", Color.White);
+        primaryAlpha = template.color.alpha;
+        template.color.alpha = 0f;
+
+        renderer = new SerialImageGroup(template, d.Attr("textures","ChroniaHelper/DisplayFonts/font").Split(',',StringSplitOptions.TrimEntries));
+        renderer.groupOrigin = new Vc2(d.Float("originX", 0.5f), d.Float("originY", 0.5f));
+        renderer.memberDistance = d.Float("lineDistance", 2f);
+
+        content = d.Attr("dialogID");
+
+        parallax = new Vc2(d.Float("parallaxX", 1f), d.Float("parallaxY", 1f));
+        staticScreen = new Vc2(d.Float("screenX", 160f), d.Float("screenY", 90f));
+        
+        typingDisplay = d.Bool("typewriterEffect", false);
+        fadeInSpeed = d.Float("fadeInSpeed", 4f);
+        fadeOutSpeed = d.Float("fadeOutSpeed", 2f);
+        letterInterval = d.Float("letterDisplayInterval", 0.1f).ClampMin(Engine.DeltaTime);
+
+        overrideFlag = d.Attr("triggerFlag");
+        hasOverrideFlag = !overrideFlag.IsNullOrEmpty();
+
+        reference = d.Attr("characterReference", "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/.<>()[]{}'\"?!\\:; =,");
+
+        leaveReset = d.Bool("leaveReset", false);
+
+        Collider = new Hitbox(d.Width, d.Height);
+    }
+    public SerialImageGroup renderer;
+    public string content;
+    public Vc2 parallax = Vc2.Zero;
+    public Vc2 staticScreen = Vc2.Zero;
+    private bool typingDisplay = true;
+    public float fadeInSpeed = 4f, fadeOutSpeed = 2f;
+    public float primaryAlpha = 1f;
+    public float letterInterval = 0.1f;
+    public string overrideFlag;
+    private bool hasOverrideFlag = false;
+    public bool leaveReset = false;
+
+    public string reference = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/.<>()[]{}'\"?!\\:; =,";
+
+    public List<string> ParseRenderTarget()
+    {
+        string text = Dialog.Clean(content, Dialog.Languages["english"]);
+        
+        var lines = text.Split(new char[] { '\n', '\r'}, StringSplitOptions.TrimEntries);
+        var result = new List<string>();
+        foreach (string line in lines)
+        {
+            string trimmed = line.Trim();
+            if (!string.IsNullOrEmpty(trimmed))
+            {
+                result.Add(trimmed);
+            }
+        }
+        
+        return result;
+    }
+
+    public int Reflection(char c)
+    {
+        return reference.Contains(c) ? reference.IndexOf(c) : reference.IndexOf(" ");
+    }
+    
+    List<string> progressedText = new();
+    List<int> progress = new();
+    public override void Render()
+    {
+        base.Render();
+
+        List<string> orig = ParseRenderTarget();
+
+        // set up typer text
+        if (!typingDisplay) { progressedText = orig; }
+        else
+        {
+            if (!renderArg && fadeEnded)
+            {
+                progressedText = new();
+                progress = new();
+                for (int i = 0; i < orig.Count; i++)
+                {
+                    progressedText.Add("");
+                    progress.Add(-1);
+                }
+            }
+            else if (renderArg)
+            {
+                if (Scene.OnInterval(letterInterval))
+                {
+                    for (int i = 0; i < progress.Count; i++)
+                    {
+                        if (i == 0)
+                        {
+                            if (progress[0] < orig[0].Length - 1) { progress[0]++; }
+
+                            progressedText[0] = orig[0].Substring(0, progress[0] + 1);
+                            continue;
+                        }
+
+                        if (progress[i] < orig[i].Length - 1 && progress[i - 1] == orig[i - 1].Length - 1)
+                        {
+                            progress[i]++;
+                        }
+
+                        progressedText[i] = orig[i].Substring(0, progress[i] + 1);
+                    }
+                }
+            }
+        }
+        
+        renderer.Render(progressedText,
+            (c) => Reflection(c),
+            nodes[1].InParallax(parallax, staticScreen));
+    }
+    public bool renderArg => inRange || hasOverrideFlag && overrideFlag.GetFlag();
+
+    public bool collided = false;
+    public bool inRange = false;
+    public bool fadeEnded = false;
+    public override void Update()
+    {
+        base.Update();
+        
+        if(PUt.TryGetPlayer(out Player player))
+        {
+            collided = CollideCheck(player);
+        }
+        else
+        {
+            collided = false;
+        }
+
+        inRange = leaveReset ? collided : inRange.TryPositive(collided);
+        
+        renderer.template.color.alpha = Calc.Approach(renderer.template.color.alpha,
+            updateArg ? primaryAlpha : 0f,
+            (updateArg ? fadeInSpeed : fadeOutSpeed) * Engine.DeltaTime
+            );
+        fadeEnded = renderer.template.color.alpha == (updateArg ? primaryAlpha : 0f);
+    }
+    
+    public bool updateArg => inRange || hasOverrideFlag && overrideFlag.GetFlag();
+}
---
+++ ModFolder/Loenn/entities/MessageDisplayZone.lua
@@ -0,0 +1,138 @@
+local utils = require('utils')
+local vivUtilsMig = require('mods').requireFromPlugin('libraries.vivUtilsMig')
+local drawableSprite = require("structs.drawable_sprite")
+local drawableLine = require("structs.drawable_line")
+local drawing = require("utils.drawing")
+
+local renderer = {
+    name = "ChroniaHelper/MessageDisplayZone"
+}
+
+renderer.nodeLimits = {1,1}
+
+renderer.placements = {
+    name = "renderer",
+    data = {
+        width = 16,
+        height = 16,
+        textures = "ChroniaHelper/DisplayFonts/font",
+        characterReference = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/.<>()[]{}'\"?!\\:; =,",
+        dialogID = "",
+        lineOriginX = 0.5,
+        lineOriginY = 0.5,
+        originX = 0.5,
+        originY = 0.5,
+        parallaxX = 1,
+        parallaxY = 1,
+        screenX = 160,
+        screenY = 90,
+        renderMode = 0,
+        lineDistance = 0,
+        letterDistance = 0,
+        fadeInSpeed = 4,
+        fadeOutSpeed = 2,
+        letterDisplayInterval = 0.1,
+        fontColor = "ffffff",
+        triggerFlag = "",
+        typewriterEffect = false,
+        leaveReset = false,
+    }, 
+    --nodeLimits = {0,2}
+}
+
+renderer.fieldInformation = {
+    textures = {
+        fieldType = "list",
+        minimumElements = 1,
+    },
+    screenX = {
+        fieldType = "integer",
+    },
+    screenY = {
+        fieldType = "integer",
+    },
+    letterDistance = {
+        fieldType = "integer",
+    },
+    lineDistance = {
+        fieldType = "integer",
+    },
+    sourcePath = {
+        options = {
+            "ChroniaHelper/StopclockFonts/font",
+            "ChroniaHelper/StopclockFonts/fontB",
+            ["(Different Sized Test Sprite)"] = "ChroniaHelper/StopclockFonts/differentSized/font",
+        },
+        editable = true,
+    },
+    positionAlign = {
+        fieldType = "integer",
+        options = {
+            ["center"] = 5,
+            ["top left"] = 1,
+            ["top center"] = 2,
+            ["top right"] = 3,
+            ["center left"] = 4,
+            ["center right"] = 6,
+            ["bottom left"] = 7,
+            ["bottom center"] = 8,
+            ["bottom right"] = 9,
+        },
+        editable = false,
+    },
+    segmentAlign = {
+        fieldType = "integer",
+        options = {
+            ["center"] = 5,
+            ["top left"] = 1,
+            ["top center"] = 2,
+            ["top right"] = 3,
+            ["center left"] = 4,
+            ["center right"] = 6,
+            ["bottom left"] = 7,
+            ["bottom center"] = 8,
+            ["bottom right"] = 9,
+        },
+        editable = false,
+    },
+    renderMode = {
+        fieldType = "integer",
+        options = {
+            ["Compact"] = 0,
+            ["Equal Distance"] = 1,
+        },
+        editable = false,
+    },
+    rendererColor = {
+        fieldType = "color",
+        useAlpha = true,
+    },
+    maximumUnit = {
+        fieldType = "integer",
+        options = { ["Year"] = 6, ["Month"] = 5, ["Day"] = 4, ["Hour"] = 3, ["Minute"] = 2, ["Second"] = 1, ["Millisecond"] = 0 },
+        editable = false,
+    },
+    minimumUnit = {
+        fieldType = "integer",
+        options = { ["Year"] = 6, ["Month"] = 5, ["Day"] = 4, ["Hour"] = 3, ["Minute"] = 2, ["Second"] = 1, ["Millisecond"] = 0 },
+        editable = false,
+    },
+    depth = require('mods').requireFromPlugin('helpers.field_options').depths,
+}
+
+renderer.sprite = function(room, entity, viewport)
+    return {
+        require("structs.drawable_rectangle").fromRectangle("bordered", entity.x, entity.y, entity.width, entity.height,
+            {1,1,1,0.3}, {1,1,1,1}),
+    }
+end
+
+renderer.nodeSprite = function(room, entity, node, nodeIndex, viewport)
+    return {
+        require("structs.drawable_rectangle").fromRectangle("bordered", node.x - 4, node.y - 4, 8, 8,
+            {1,1,1,0.3}, {1,1,1,1}),
+        drawableLine.fromPoints({node.x, node.y, entity.x + entity.width / 2, entity.y + entity.height / 2}, 1)
+    }
+end
+
+return renderer
