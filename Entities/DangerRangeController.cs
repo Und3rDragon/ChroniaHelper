93f6c5ae34d43714067925fa7181bcd8e491a66b
UnderDragon <1306094605@qq.com>
2025-11-04 00:28:23 +0800

    Danger Range Controller

---
+++ Entities/DangerRangeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Celeste.Mod.Entities;
+using ChroniaHelper.Cores;
+using ChroniaHelper.Utils;
+using ChroniaHelper.Utils.ChroniaSystem;
+
+namespace ChroniaHelper.Entities;
+
+[CustomEntity("ChroniaHelper/DangerRangeController")]
+public class DangerRangeController : BaseEntity
+{
+    public DangerRangeController(EntityData data, Vc2 offset) : base(data, offset)
+    {
+        flag = data.Attr("flag");
+        isX = data.Bool("calculateX", false);
+        isY = data.Bool("calculateY", false);
+        value = data.Float("value", 0f);
+        greater = data.Bool("greaterThan", true);
+    }
+    public string flag;
+    public bool isX, isY;
+    public float value;
+    public bool greater;
+
+    protected override void UpdateExecute()
+    {
+        if (flag.IsNotNullOrEmpty() && !flag.GetFlag()) { return; }
+        
+        if(PUt.TryGetPlayer(out Player player))
+        {
+            bool argX = greater ? player.Center.X > value : player.Center.X < value;
+            bool argY = greater ? player.Center.Y > value : player.Center.Y < value;
+            bool arg = (isX && argX) || (argY && isY);
+            Log.Info(argX, argY, isX && argX, argY && isY, arg);
+            
+            if (arg) { player.Die(player.Speed.SafeNormalize()); }
+        }
+    }
+}
---
+++ ModFolder/Loenn/entities/DangerRangeController.lua
@@ -0,0 +1,38 @@
+local drawableRectangle = require("structs.drawable_rectangle")
+local utils = require("utils")
+local drawableSprite = require("structs.drawable_sprite")
+
+local controller = {}
+
+controller.name = "ChroniaHelper/DangerRangeController"
+
+controller.placements = {
+    name = "controller",
+    data = {
+        flag = "",
+        calculateX = false,
+        calculateY = false,
+        value = 0,
+        greaterThan = true,
+    },
+}
+
+controller.ignoredFields = {
+    "_x", "_y", "x", "y", "_id", "_name"
+}
+
+controller.fieldInformation = 
+{
+    
+}
+
+function controller.sprite(room, entity)
+    local sprite = {}
+    local rect = drawableRectangle.fromRectangle("fill", entity.x, entity.y, 16, 16, {0.0, 0.0, 0.0})
+    local iconSprite = drawableSprite.fromTexture("ChroniaHelper/LoennIcons/RoomTag", entity)
+
+    table.insert(sprite, iconSprite)
+    return sprite
+end
+
+return controller
