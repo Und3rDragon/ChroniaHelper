7d101f76e0f3ca5e26d3b10a177b4001db1c4160
UnderDragon <1306094605@qq.com>
2025-11-16 19:58:48 +0800

    Cleanup

--- Modules/ChroniaHelperGlobalSaveData.cs
+++ Modules/ChroniaHelperGlobalSaveData.cs
@@ -10,14 +10,5 @@ namespace ChroniaHelper.Modules;
 
 public class ChroniaHelperGlobalSaveData : ChroniaHelperModuleGlobalSaveData
 {
-    public string Name { get; set; } = "123";
-    public int hhh { get; set; } = 2;
-
-    [ChroniaGlobalSavePath]
-    public bool lol { get; set; } = false;
-    [ChroniaGlobalSavePath]
-    public int hhh2 { get; set; } = 3;
-
-    [ChroniaGlobalSavePath("Gaming/Haha.xml")]
-    public int hhh3 { get; set; } = 4;
+    
 }
