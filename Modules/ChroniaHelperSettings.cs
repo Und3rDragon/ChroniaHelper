d07a554bd2304c7b22fb79033e415d1bda8e12d8
UnderDragon <1306094605@qq.com>
2025-11-23 17:38:51 +0800

    1.40.14 Displayers Controller optimize

--- Modules/ChroniaHelperSettings.cs
+++ Modules/ChroniaHelperSettings.cs
@@ -8,9 +8,8 @@ public class ChroniaHelperSettings : EverestModuleSettings
 {
     public enum DisplayPosition { PlayerBased, StaticScreen }
     public enum Aligning { Left, Middle, Right }
-
-    [SettingSubMenu]
-    public class StaminaDisplayer
+    
+    public class CommonDisplayer
     {
         public bool enabled { get; set; } = false;
         public DisplayPosition displayPosition { get; set; } = DisplayPosition.PlayerBased;
@@ -23,230 +22,109 @@ public class ChroniaHelperSettings : EverestModuleSettings
         [SettingRange(0, 1000, true)]
         public int scale { get; set; } = 60;
         public Aligning aligning { get; set; } = Aligning.Middle;
+    }
+
+    [SettingSubMenu]
+    public class StaminaDisplayer : CommonDisplayer
+    {
+        
     }
 
     public StaminaDisplayer staminaMeterMenu { get; set; } = new();
 
     [SettingSubMenu]
-    public class DashesDisplayer
+    public class DashesDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.PlayerBased;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 0;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = -16;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 60;
-        public Aligning aligning { get; set; } = Aligning.Middle;
+        
     }
 
     public DashesDisplayer dashesCounter { get; set; } = new();
 
     [SettingSubMenu]
-    public class RealTimeClockDisplayer
+    public class RealTimeClockDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
         public bool hasSeconds { get; set; } = true;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 160;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 172;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 60;
-        public Aligning aligning { get; set; } = Aligning.Middle;
     }
 
     public RealTimeClockDisplayer realTimeClock { get; set; } = new();
 
     [SettingSubMenu]
-    public class StateMachineDisplayer
+    public class StateMachineDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 160;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 165;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 60;
-        public Aligning aligning { get; set; } = Aligning.Middle;
+        
     }
 
     public StateMachineDisplayer stateMachineDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class SpeedDisplayer
+    public class SpeedDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
         public enum SpeedDisplay { speedX, speedY, speedToCoordinates, speedAll }
         public SpeedDisplay speedDisplay { get; set; } = SpeedDisplay.speedAll;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.PlayerBased;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 0;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = -16;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 60;
-        public Aligning aligning { get; set; } = Aligning.Middle;
     }
 
     public SpeedDisplayer speedDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class PlayerSpriteDisplayer
+    public class PlayerSpriteDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
         public enum DisplaySprite { Animation, File, Comparison}
         public DisplaySprite displaySprite { get; set; } = DisplaySprite.Comparison;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 160;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 165;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 60;
-        public Aligning aligning { get; set; } = Aligning.Middle;
     }
     public PlayerSpriteDisplayer playerSpriteDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class PlayerPositionDisplayer
+    public class PlayerPositionDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
         public enum DisplayCoordinates { X, Y, Both }
         public DisplayCoordinates displayCoordinates { get; set; } = DisplayCoordinates.Both;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
         public bool useGlobalCoordinates { get; set; } = true;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 160;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 175;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 45;
-        public Aligning aligning { get; set; } = Aligning.Middle;
     }
     public PlayerPositionDisplayer playerPositionDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class DeathsDisplayer
+    public class DeathsDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 318;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 19;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 60;
-        public Aligning aligning { get; set; } = Aligning.Right;
+        
     }
 
     public DeathsDisplayer deathsDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class TotalDeathsDisplayer
+    public class TotalDeathsDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 318;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 8;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 55;
-        public Aligning aligning { get; set; } = Aligning.Right;
+        
     }
 
     public TotalDeathsDisplayer totalDeathsDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class SaveFileDeathsDisplayer
+    public class SaveFileDeathsDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 318;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 8;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 55;
-        public Aligning aligning { get; set; } = Aligning.Right;
+        
     }
 
     public SaveFileDeathsDisplayer saveFileDeathsDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class MapNameDisplayer
+    public class MapNameDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
         public bool prefix { get; set; } = false;
         public bool suffixAuthor { get; set; } = false;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 160;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 175;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = -2;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 30;
-        public Aligning aligning { get; set; } = Aligning.Middle;
     }
     public MapNameDisplayer mapNameDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class RoomNameDisplayer
+    public class RoomNameDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
         public bool prefix { get; set; } = false;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 160;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 168;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 40;
-        public Aligning aligning { get; set; } = Aligning.Middle;
     }
     public RoomNameDisplayer roomNameDisplayer { get; set; } = new();
 
     [SettingSubMenu]
-    public class MapAuthorNameDisplayer
+    public class MapAuthorNameDisplayer : CommonDisplayer
     {
-        public bool enabled { get; set; } = false;
         public bool prefix { get; set; } = false;
-        public DisplayPosition displayPosition { get; set; } = DisplayPosition.StaticScreen;
-        [SettingRange(-1000, 1000, true)]
-        public int X { get; set; } = 160;
-        [SettingRange(-1000, 1000, true)]
-        public int Y { get; set; } = 168;
-        [SettingRange(-32, 32)]
-        public int letterDistance { get; set; } = 0;
-        [SettingRange(0, 1000, true)]
-        public int scale { get; set; } = 40;
-        public Aligning aligning { get; set; } = Aligning.Middle;
     }
     public MapAuthorNameDisplayer mapAuthorNameDisplayer { get; set; } = new();
 }
