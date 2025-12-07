0c2abcc456169effa0ecf5d9f193e8f4a0dfe8df
UnderDragon <1306094605@qq.com>
2025-12-08 00:05:06 +0800

    LoadManager optimization

--- Cores/LoadingManager.cs
+++ Cores/LoadingManager.cs
@@ -7,19 +7,23 @@ public class LoadingManager
 {
     public static void Load()
     {
-        Execute(typeof(LoadHook));
+        Execute(typeof(LoadHook), "ChroniaHelper");
     }
 
     public static void Unload()
     {
-        Execute(typeof(UnloadHook));
+        Execute(typeof(UnloadHook), "ChroniaHelper");
     }
-
-    private static void Execute(Type attributeType)
+    
+    private static void Execute(Type attributeType, string targetNamespace = null)
     {
         Type[] types = Assembly.GetExecutingAssembly().GetTypesSafe();
+
         foreach (var t in types)
         {
+            if (!string.IsNullOrEmpty(targetNamespace) && !t.FullName.StartsWith(targetNamespace))
+                continue;
+
             MethodInfo[] methods = t.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
             foreach (var method in methods)
             {
