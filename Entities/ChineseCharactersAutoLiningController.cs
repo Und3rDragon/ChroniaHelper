56dcfc69cbbeb3d019034c301660e78cb6b189a4
UnderDragon <1306094605@qq.com>
2025-12-15 14:19:31 +0800

    CN Autolining defaults modified + Controller

---
+++ Entities/ChineseCharactersAutoLiningController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Celeste.Mod.Entities;
+using ChroniaHelper.Cores;
+
+namespace ChroniaHelper.Entities;
+
+[CustomEntity("ChroniaHelper/ChineseCharactersAutoLiningController")]
+public class ChineseCharactersAutoLiningController :  BaseEntity
+{
+    public ChineseCharactersAutoLiningController(EntityData d, Vc2 o) : base(d, o)
+    {
+        state = d.Bool("autoLining", false);
+    }
+    private bool state;
+
+    public override void Added(Scene scene)
+    {
+        base.Added(scene);
+
+        Md.Settings.ChineseCharactersAutoLining = state;
+    }
+}
---
+++ ModFolder/Loenn/entities/ChineseCharactersAutoLiningController.lua
@@ -0,0 +1,36 @@
+local drawableRectangle = require("structs.drawable_rectangle")
+local utils = require("utils")
+local drawableSprite = require("structs.drawable_sprite")
+
+local controller = {}
+
+controller.name = "ChroniaHelper/ChineseCharactersAutoLiningController"
+
+controller.placements = {
+    name = "controller",
+    data = {
+        x = 0,
+        y = 0,
+        autoLining = false,
+    },
+}
+
+controller.ignoredFields = {
+    
+}
+
+controller.fieldInformation = 
+{
+    
+}
+
+function controller.sprite(room, entity)
+    local sprite = {}
+    local rect = drawableRectangle.fromRectangle("fill", entity.x, entity.y, 16, 16, {0.0, 0.0, 0.0})
+    local iconSprite = drawableSprite.fromTexture("ChroniaHelper/LoennIcons/CNAutolining", entity)
+
+    table.insert(sprite, iconSprite)
+    return sprite
+end
+
+return controller
