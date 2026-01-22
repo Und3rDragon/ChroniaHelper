d831c36ae09441053d9d9b1069a80cd8cfcf415e
UnderDragon <1306094605@qq.com>
2026-01-22 23:08:07 +0800

    Small Timer Component for Entities or such to use in codes

---
+++ Components/SimpleTimers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChroniaHelper.Cores;
+using ChroniaHelper.Utils;
+
+namespace ChroniaHelper.Components;
+
+public class SimpleTimers
+{
+    public class TimeSpanTimer : BaseComponent
+    {
+        public TimeSpan Timer => DateTime.Now - _start;
+
+        private DateTime _start = DateTime.Now;
+
+        public void Restart()
+        {
+            _start = DateTime.Now;
+        }
+    }
+
+    public class ClassicTimer : BaseComponent
+    {
+        public float Seconds = 0f;
+        
+        public bool UseRawDeltaTime = false;
+
+        public TimeSpan Timer => TimeSpan.FromMilliseconds(Seconds.ClampMin(0f) * 1000);
+
+        public ClassicTimer(bool useRaw = false)
+        {
+            Seconds = 0f;
+            UseRawDeltaTime = useRaw;
+        }
+
+        public override void Update()
+        {
+            Seconds += (UseRawDeltaTime ? Engine.RawDeltaTime : Engine.DeltaTime);
+        }
+    }
+}
