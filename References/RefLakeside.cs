6a66bc732e84322dd5a506b2a4e2cfc834ffd2c7
UnderDragon <1306094605@qq.com>
2026-02-26 02:49:40 +0800

    1.47.15 Lakeside fish pricer completed

--- References/RefLakeside.cs
+++ References/RefLakeside.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Celeste.Mod.LakeSideCode;
 using Celeste.Mod.LakeSideCode.FishDefs;
+using ChroniaHelper.Utils;
 
 namespace ChroniaHelper.References;
 
@@ -32,22 +33,89 @@ public static class RefLakeside
 
     public static Dictionary<FishType, int> FishCounters => GetSession().CatchCounter;
 
-    public static int FishPrice(FishType fish) => (fish) switch 
-    { 
-        FishType.Nothing => 0,
-        FishType.Coin => 100,
-        FishType.Bass => 120,
-        FishType.Trout => 140,
-        FishType.Spring => 160,
-        FishType.Stone => 180,
-        FishType.StoneEater => 200,
-        FishType.Blahaj => 220,
-        FishType.Bomb => 250,
-        FishType.Leaf => 280,
-        FishType.Angel => 350,
-        FishType.Devil => 400,
-        FishType.Cooked => 600,
-        FishType.Mythic => 1000,
-        _ => 0,
-    };
+    public class FishPrices
+    {
+        public Dictionary<int, int> PriceList = new()
+        {
+            { (int)FishType.Nothing, 0 },
+            { (int)FishType.Coin, 20 },
+            { (int)FishType.Bass, 100 },
+            { (int)FishType.Trout, 100 },
+            { (int)FishType.Spring, 150 },
+            { (int)FishType.Stone, 180 },
+            { (int)FishType.StoneEater, 200 },
+            { (int)FishType.Blahaj, 220 },
+            { (int)FishType.Bomb, 250 },
+            { (int)FishType.Leaf, 280 },
+            { (int)FishType.Angel, 350 },
+            { (int)FishType.Devil, 400 },
+            { (int)FishType.Cooked, 600 },
+            { (int)FishType.Mythic, 1000 },
+        };
+
+        public Dictionary<int, int> Variations = new()
+        {
+            { (int)FishType.Nothing, 0 },
+            { (int)FishType.Coin, 0 },
+            { (int)FishType.Bass, 10 },
+            { (int)FishType.Trout, 10 },
+            { (int)FishType.Spring, 20 },
+            { (int)FishType.Stone, 20 },
+            { (int)FishType.StoneEater, 40 },
+            { (int)FishType.Blahaj, 40 },
+            { (int)FishType.Bomb, 50 },
+            { (int)FishType.Leaf, 80 },
+            { (int)FishType.Angel, 100 },
+            { (int)FishType.Devil, 100 },
+            { (int)FishType.Cooked, 150 },
+            { (int)FishType.Mythic, 200 },
+        };
+
+        public Dictionary<int, List<int>> PriceEntries = new();
+
+        public void GeneratePriceList(Dictionary<FishType, int> counter)
+        {
+            foreach(var item in counter)
+            {
+                int type = (int)item.Key;
+                int count = item.Value;
+
+                for(int i = 0; i < count; i++)
+                {
+                    PriceEntries[type].Create(i, 
+                        PriceList[type] + RandomUtils.RandomInt(Variations[type]) - Variations[type] / 2,
+                        PriceList[type] + RandomUtils.RandomInt(Variations[type]) - Variations[type] / 2
+                    );
+                }
+            }
+        }
+
+        public int GetTotalCredits()
+        {
+            int total = 0;
+            foreach(var item in PriceEntries)
+            {
+                int credits = 0;
+                foreach(var entry in item.Value)
+                {
+                    credits += entry;
+                }
+
+                total += credits;
+            }
+
+            return total;
+        }
+
+        public int GetToralCredits(FishType fish)
+        {
+            int total = 0;
+            foreach (var item in PriceEntries.GetValueOrDefault((int)fish, new()))
+            {
+                total += item;
+            }
+
+            return total;
+        }
+    }
 }
