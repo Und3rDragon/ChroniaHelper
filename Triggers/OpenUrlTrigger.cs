92e76198d1ee51beceb5ca9981476cd328e5ad35
UnderDragon <1306094605@qq.com>
2026-02-27 02:34:42 +0800

    1.47.20 Gamation URL validation prompt return

--- Triggers/OpenUrlTrigger.cs
+++ Triggers/OpenUrlTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Celeste.Mod.Entities;
 using Celeste.Mod.XaphanHelper.UI_Elements;
+using ChroniaHelper.Cores;
 
 namespace ChroniaHelper.Triggers;
 
@@ -20,40 +21,37 @@ public sealed class OpenUrlTrigger : Trigger
         loadOnce = data.Bool("loadOnce", false);
         url = data.Attr("url", "https://www.celestegame.com/");
 
-        // Ensure a URL is being opened and not arbitrary code
-        //if (Uri.TryCreate(url, UriKind.Absolute, out Uri validURL))
-        //{
-        //    safe = (validURL.Scheme == Uri.UriSchemeHttp || validURL.Scheme == Uri.UriSchemeHttps);
-        //}
-        //else safe = false;
+        LinkValidation();
     }
 
     public override void OnEnter(Player player)
     {
         base.OnEnter(player);
 
-        //if (safe) { 
-        //    try
-        //    {
-        //        ProcessStartInfo info = new(url) { UseShellExecute = true };
-        //        Process.Start(info);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Logger.LogDetailed(e, "OpenUrlHelper");
-        //    }
-        //}
-        //else
-        //{
-        //    Logger.Error("OpenUrlHelper", "Invalid URL! It should match the \"https://www.example.com\" format.");
-        //}
-
-        GenerateUI();
+        if (safe)
+        {
+            GenerateUI();
+        }
+        else
+        {
+            Logger.Error("OpenUrlHelper", "Invalid URL! It should match the \"https://www.example.com\" format.");
+        }
 
         if (once)
             RemoveSelf();
         if (loadOnce)
             SceneAs<Level>().Session.DoNotLoad.Add(id);
+    }
+
+    [Credits("Gamation")]
+    public void LinkValidation()
+    {
+        // Ensure a URL is being opened and not arbitrary code
+        if (Uri.TryCreate(url, UriKind.Absolute, out Uri validURL))
+        {
+            safe = (validURL.Scheme == Uri.UriSchemeHttp || validURL.Scheme == Uri.UriSchemeHttps);
+        }
+        else safe = false;
     }
 
     public void GenerateUI()
