68deb401a448d4d9d64651f100cb7a4a24fd7e3e
UnderDragon <1306094605@qq.com>
2026-02-27 02:23:30 +0800

    1.47.19 An ancient problem fix: Open Url Trigger UI

--- Triggers/OpenUrlTrigger.cs
+++ Triggers/OpenUrlTrigger.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Celeste.Mod.Entities;
+using Celeste.Mod.XaphanHelper.UI_Elements;
 
 namespace ChroniaHelper.Triggers;
 
@@ -20,34 +21,76 @@ public sealed class OpenUrlTrigger : Trigger
         url = data.Attr("url", "https://www.celestegame.com/");
 
         // Ensure a URL is being opened and not arbitrary code
-        if (Uri.TryCreate(url, UriKind.Absolute, out Uri validURL))
-        {
-            safe = (validURL.Scheme == Uri.UriSchemeHttp || validURL.Scheme == Uri.UriSchemeHttps);
-        }
-        else safe = false;
+        //if (Uri.TryCreate(url, UriKind.Absolute, out Uri validURL))
+        //{
+        //    safe = (validURL.Scheme == Uri.UriSchemeHttp || validURL.Scheme == Uri.UriSchemeHttps);
+        //}
+        //else safe = false;
     }
 
     public override void OnEnter(Player player)
     {
         base.OnEnter(player);
-        if (safe) { 
-            try
-            {
-                ProcessStartInfo info = new(url) { UseShellExecute = true };
-                Process.Start(info);
-            }
-            catch (Exception e)
-            {
-                Logger.LogDetailed(e, "OpenUrlHelper");
-            }
-        }
-        else
-        {
-            Logger.Error("OpenUrlHelper", "Invalid URL! It should match the \"https://www.example.com\" format.");
-        }
+
+        //if (safe) { 
+        //    try
+        //    {
+        //        ProcessStartInfo info = new(url) { UseShellExecute = true };
+        //        Process.Start(info);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Logger.LogDetailed(e, "OpenUrlHelper");
+        //    }
+        //}
+        //else
+        //{
+        //    Logger.Error("OpenUrlHelper", "Invalid URL! It should match the \"https://www.example.com\" format.");
+        //}
+
+        GenerateUI();
+
         if (once)
             RemoveSelf();
         if (loadOnce)
             SceneAs<Level>().Session.DoNotLoad.Add(id);
+    }
+
+    public void GenerateUI()
+    {
+        MaP.level.Paused = true;
+        
+        TextMenu menu = new();
+        menu.AutoScroll = false;
+        menu.Position = new Vector2((float)Engine.Width / 2f, (float)Engine.Height / 2f - 100f);
+        menu.Add(new TextMenu.Header("Link redirect?"));
+        menu.Add(new TextMenu.SubHeader("Are you familiar with the link?", false));
+        menu.Add(new TextMenu.SubHeader(url, false));
+        menu.Add(new TextMenu.Button("Confirm").Pressed(delegate
+        {
+            ProcessStartInfo info = new(url) { UseShellExecute = true };
+            Process.Start(info);
+            menu.OnCancel();
+        }));
+        menu.Add(new TextMenu.Button("Cancel").Pressed(delegate
+        {
+            menu.OnCancel();
+        }));
+        menu.OnPause = (menu.OnESC = delegate
+        {
+            menu.RemoveSelf();
+
+            MaP.level.Paused = false;
+            MaP.level.unpauseTimer = 0.15f;
+            Audio.Play("event:/ui/game/unpause");
+        });
+        menu.OnCancel = delegate
+        {
+            Audio.Play("event:/ui/main/button_back");
+            menu.RemoveSelf();
+            MaP.level.Paused = false;
+        };
+
+        MaP.level.Add(menu);
     }
 }
