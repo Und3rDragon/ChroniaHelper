173344ccdbbc056dd5ca622be69a715df3c6b5d8
UnderDragon <1306094605@qq.com>
2026-03-17 16:11:20 +0800

    1.47.54

--- ModFolder/Loenn/entities/CustomShield.lua
+++ ModFolder/Loenn/entities/CustomShield.lua
@@ -42,6 +42,7 @@ shield.sprite = function(room, entity)
     end
 
     if not entity.square then
+        -- drawCircle函数来自于Refill.lua
         return drawCircle({entity.x, entity.y, entity.x + r, entity.y}, 1)
     else
         return require("structs.drawable_rectangle").fromRectangle("bordered", entity.x - r, entity.y - r, 2 * r, 2 * r, {1,1,1,0.1}, {1,1,1,1})
@@ -55,45 +56,6 @@ shield.selection = function(room, entity)
     end
 
     return require("utils").rectangle(entity.x - r, entity.y - r, r * 2, r * 2)
-end
-
--- 定义画圆函数
-function getDrawCircle(entity, res)
-    local sprites = {}
-
-    local r = tonumber(entity.radius)
-    if r == nil then
-        r = 8
-    end
-
-    -- 计算每个圆周片段的角度大小
-    local angleIncrement = (2 * math.pi) / res
-    
-    -- 初始化前一个点为圆的起点
-    local prevPoint = {x = entity.x + r, y = entity.y}
-    
-    -- 遍历所有圆周点
-    for i = 1, res do
-        -- 当前角度
-        local theta = i * angleIncrement
-        
-        -- 计算当前点的位置
-        local currentPoint = {
-            x = entity.x + r * math.cos(theta),
-            y = entity.y + r * math.sin(theta)
-        }
-        
-        -- 绘制从上一点到当前点的线段
-        table.insert(sprites, drawableLine.fromPoints(prevPoint, currentPoint))
-        
-        -- 更新前一个点为当前点
-        prevPoint = currentPoint
-    end
-    
-    -- 最后还需连接最后一个点和第一个点，以闭合图形
-    table.insert(sprites, drawableLine.fromPoints(prevPoint, {x = entity.x + r, y = entity.y}))
-
-    return sprites
 end
 
 return shield
