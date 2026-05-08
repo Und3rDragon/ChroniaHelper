011e98a3444748cdb71665d22abe10c01051cdfc
UnderDragon <1306094605@qq.com>
2026-05-08 23:47:05 +0800

    1.49.29 Message Displayer direct use

--- Entities/MessageDisplayerNormal.cs
+++ Entities/MessageDisplayerNormal.cs
@@ -67,9 +67,17 @@ public class MessageDisplayerNormal : SerialImageRenderer
     public string reference = Cons.DisplayFontsReference;
     public List<string> ParseRenderTarget()
     {
-        string text = content.StartsWith("#") ?
-            Md.Session.keystrings.GetValueOrDefault(content.TrimStart('#'), "") :
-            content.ParseDialogToString(Languages.English);
+        string text = content.ParseDialogToString(Languages.English);
+        bool arg1 = content.StartsWith("\"") && content.EndsWith("\"");
+        bool arg2 = content.StartsWith("#");
+        if (arg1)
+        {
+            text = content.TrimStart("\"").TrimEnd("\"").ToString();
+        }
+        else if (arg2)
+        {
+            text = Md.Session.keystrings.GetValueOrDefault(content.TrimStart('#'), "");
+        }
 
         var lines = text.Split(new char[] { '\n', '\r'}, StringSplitOptions.TrimEntries);
         var result = new List<string>();
