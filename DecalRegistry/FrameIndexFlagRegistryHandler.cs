21d75760e24a2cf61616f130b793c8011f31f136
UnderDragon <1306094605@qq.com>
2026-06-03 14:01:41 +0800

    1.51.25 Index Flags registry

---
+++ DecalRegistry/FrameIndexFlagRegistryHandler.cs
@@ -0,0 +1,61 @@
+﻿using Celeste.Mod.Registry.DecalRegistryHandlers;
+using ChroniaHelper.Cores;
+using ChroniaHelper.Utils.ChroniaSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ChroniaHelper.DecalRegistry;
+
+public class FrameIndexFlagRegistryHandler : DecalRegistryHandler
+{
+    [LoadHook]
+    public static void Load()
+    {
+        Celeste.Mod.DecalRegistry.AddPropertyHandler<FrameIndexFlagRegistryHandler>();
+    }
+
+    public override string Name => "chronia.frameIndexFlag";
+
+    public override void Parse(XmlAttributeCollection xml)
+    {
+        Handler = new(
+            GetCSVIntWithTricks(xml, "indexes", ""),
+            GetString(xml, "flags", ""));
+    }
+
+    public override void ApplyTo(Decal decal)
+    {
+        Handler.AddTo(decal);
+    }
+
+    public FrameIndexFlagRegistry Handler;
+}
+
+public class FrameIndexFlagRegistry : BaseComponent
+{
+    public FrameIndexFlagRegistry(int[] targets, string flags)
+    {
+        Targets = targets.ToList();
+        FlagExpression = flags;
+    }
+    public List<int> Targets;
+    public string FlagExpression;
+
+    public override void Update()
+    {
+        if (base.Entity?.Scene == null) { return; }
+
+        if (base.Entity is not Decal) { return; }
+
+        Decal decal = base.Entity as Decal;
+
+        if (Targets.Contains((int)decal.frame))
+        {
+            FlagExpression.SetGeneralFlags();
+        }
+    }
+}
