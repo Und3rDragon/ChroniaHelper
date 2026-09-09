312246883b0de5fb395564c6f1de9bee7baf54c9
UnderDragon <1306094605@qq.com>
2026-09-10 01:10:43 +0800

    ChroniaHelper API coming?

---
+++ API/API.cs
@@ -0,0 +1,64 @@
+﻿using ChroniaHelper.Utils.ChroniaSystem;
+using ChroniaHelper.Utils.LogicExpression;
+using ChroniaHelper.Utils.MathExpression;
+using MonoMod.ModInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChroniaHelper.API;
+
+[ModExportName("ChroniaHelper")]
+public static class API
+{
+    public static Version APIVersion => new(1, 0);
+
+    /// <summary>
+    /// Calculate a Chronia Math Expression 
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <param name="setVariables"></param>
+    /// <param name="getFlagValue"></param>
+    /// <returns></returns>
+    public static double ParseChroniaMathExpression(string expression, Func<string, double> setVariables = null, Func<string, double> getFlagValue = null)
+        => expression.ParseMathExpressionRaw(setVariables, getFlagValue);
+    public static float ParseChroniaMathExpression(string expression, Func<string, float> setVariables = null, Func<string, float> getFlagValue = null)
+        => expression.ParseMathExpression(setVariables, getFlagValue);
+
+    /// <summary>
+    /// Calculate a Chronia Logic Expression
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <param name="setVariableValue"></param>
+    /// <param name="fallback"></param>
+    /// <returns></returns>
+    public static bool ParseChroniaFlagLogicExpression(string expression, Func<string, bool> setVariableValue = null, bool fallback = false)
+        => expression.ParseLogicExpression(setVariableValue, fallback);
+
+    /// <summary>
+    /// Utilize Chronia Flags
+    /// </summary>
+    /// <param name="flag"></param>
+    /// <param name="state"></param>
+    /// <param name="global"></param>
+    /// <param name="perDeath"></param>
+    /// <param name="perRoom"></param>
+    public static void SetChroniaFlag(string flag, bool state = true, bool global = false, bool perDeath = false, bool perRoom = false)
+        => flag.SetFlag(state, global, perDeath, perRoom);
+
+    /// <summary>
+    /// Check if a certain flag is enlisted or checking its state
+    /// </summary>
+    /// <param name="flag">Flag name</param>
+    /// <param name="isGlobal">Check if the flag is enlisted in global flags</param>
+    /// <param name="isPerDeath">Check if the flag is enlisted in per-death flags</param>
+    /// <param name="isPerRoom">Check if the flag is enlisted in per-room flags</param>
+    /// <returns></returns>
+    public static bool GetChroniaFlag(string flag, bool isGlobal = false, bool isPerDeath = false, bool isPerRoom = false)
+        => (flag.GetFlag() && !isGlobal && !isPerDeath && !isPerRoom)
+        || ((Md.SaveData?.flags.Contains(flag) ?? false) && isGlobal)
+        || ((Md.Session?.flagsPerRoom.Contains(flag) ?? false) && isPerRoom)
+        || ((Md.Session?.flagsPerDeath.Contains(flag) ?? false) && isPerDeath);
+}
